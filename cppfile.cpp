d86c5389f6b44aa79613665fa841a99360e282e4
satyam236 <satyamisro2019@gmail.com>
2025-07-03 16:52:15 +0530

    new cpp file

---
+++ cppfile.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+void solve(){
+    cout<<"hello world"<<endl;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        solve();
+    }
+}
